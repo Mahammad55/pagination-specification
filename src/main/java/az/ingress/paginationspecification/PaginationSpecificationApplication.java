2851fc7d4fa16b2105746b46f50aade6e8beb4e6
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-03-09 16:46:45 +0400

    first commit

---
+++ src/main/java/az/ingress/paginationspecification/PaginationSpecificationApplication.java
@@ -0,0 +1,13 @@
+package az.ingress.paginationspecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSpecificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/ingress/paginationspecification/PaginationSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.paginationspecification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
